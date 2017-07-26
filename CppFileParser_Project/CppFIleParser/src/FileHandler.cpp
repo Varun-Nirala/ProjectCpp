aa8c521e6fd6e6285c5f815fbbc1fce1b0299b76
Varun Nirala <nirala.varun@gmail.com>
2017-07-26 17:53:40 +0530

    Updating CppFIleParser

--- CppFileParser_Project/CppFIleParser/src/FileHandler.cpp
+++ CppFileParser_Project/CppFIleParser/src/FileHandler.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <dirent.h>
 #include <algorithm>
-#include <cstdio>
+//#include <cstdio>
 #include <functional>
 #include "FileHandler.h"
 #include "helper.h"
 
-using namespace std;
-
 // PUBLIC MEMBERS
-FileHandler::FileHandler(const string &sFilepath)
+FileHandler::FileHandler(const std::string &sFilepath)
 {
 	Initialize(sFilepath);
 }
@@ -19,18 +17,19 @@ FileHandler::~FileHandler()
 	Release();
 }
 
-string FileHandler::GetFileName() const
+std::string FileHandler::GetFileName() const
 {
 	return m_sFileName;
 }
 
-string FileHandler::GetFilePath() const
+std::string FileHandler::GetFilePath() const
 {
 	return m_sFilePath;
 }
 
-bool FileHandler::Rename(const string &sNewName)
+bool FileHandler::Rename(const std::string &sNewName)
 {
+	bool bRc = true;
 	if(SaveEditedData())
 	{
 		if(m_fstream.is_open())
@@ -40,7 +39,7 @@ bool FileHandler::Rename(const string &sNewName)
 			{
 				LOG_INFO("File renamed");
 				m_fstream.open(GetFileNameFormRoot());
-				return true;
+				bRc = true;
 			}
 		}
 		else
@@ -51,8 +50,9 @@ bool FileHandler::Rename(const string &sNewName)
 	else
 	{
 		LOG_ERROR("File can't be saved.");
-		return false;
+		bRc = false;
 	}
+	return bRc;
 }
 
 size_t FileHandler::GetSize() const
@@ -60,7 +60,7 @@ size_t FileHandler::GetSize() const
 	return m_vFileLines.size();
 }
 
-string FileHandler::GetLine(int lineNumber) const
+std::string FileHandler::GetLine(int lineNumber) const
 {
 	if(ValidRange(lineNumber))
 	{
@@ -74,12 +74,12 @@ string FileHandler::GetLine(int lineNumber) const
 	return "";
 }
 
-bool FileHandler::Replace(const string &sSrc, const string &sDst, bool bAllMatch, bool bCaseSensitiveSearch)
+bool FileHandler::Replace(const std::string &sSrc, const std::string &sDst, bool bAllMatch, bool bCaseSensitiveSearch)
 {
 	bool bRc = true;
 	if(bAllMatch)
 	{
-		vector<int> v_SearchIndexes;
+		std::vector<int> v_SearchIndexes;
 		if(SearchLine(sSrc, v_SearchIndexes, bCaseSensitiveSearch))
 		{
 			// TODO
@@ -107,7 +107,7 @@ bool FileHandler::Replace(const string &sSrc, const string &sDst, bool bAllMatch
 	return bRc;
 }
 
-bool FileHandler::AddAfter(int lineNumber, const string &sStatement)
+bool FileHandler::AddAfter(int lineNumber, const std::string &sStatement)
 {
 	if(ValidRange(lineNumber))
 	{
@@ -123,7 +123,7 @@ bool FileHandler::AddAfter(int lineNumber, const string &sStatement)
 	}
 }
 
-bool FileHandler::AddAfter(const string &sSearchLine, const string &sStatement)
+bool FileHandler::AddAfter(const std::string &sSearchLine, const std::string &sStatement)
 {
 	int index = SearchLine(sSearchLine);
 
@@ -139,7 +139,7 @@ bool FileHandler::AddAfter(const string &sSearchLine, const string &sStatement)
 	}
 }
 
-bool FileHandler::AddBefore(int lineNumber, const string &sStatement)
+bool FileHandler::AddBefore(int lineNumber, const std::string &sStatement)
 {
 	if(ValidRange(lineNumber))
 	{
@@ -158,7 +158,7 @@ bool FileHandler::AddBefore(int lineNumber, const string &sStatement)
 	}
 }
 
-bool FileHandler::AddBefore(const string &sSearchLine, const string &sStatement)
+bool FileHandler::AddBefore(const std::string &sSearchLine, const std::string &sStatement)
 {
 	int index = SearchLine(sSearchLine);
 
@@ -188,7 +188,7 @@ bool FileHandler::Remove(int lineNumber)
 	}
 }
 
-bool FileHandler::Remove(const string &sSearchLine)
+bool FileHandler::Remove(const std::string &sSearchLine)
 {
 	int lineNumber = SearchLine(sSearchLine);
 
@@ -211,12 +211,12 @@ bool FileHandler::SaveEditedData()
 		m_fstream.close();
 		
 		// Empty files current content
-		ofstream fileStream;
-		fileStream.open(GetFileNameFormRoot(), ofstream::out | ofstream::trunc);
+		std::ofstream fileStream;
+		fileStream.open(GetFileNameFormRoot(), std::ofstream::out | std::ofstream::trunc);
 
 		if(fileStream.is_open())
 		{
-			for(const string &sFileLines : m_vFileLines)
+			for(const std::string &sFileLines : m_vFileLines)
 			{
 				fileStream << sFileLines;
 			}
@@ -234,7 +234,7 @@ bool FileHandler::SaveEditedData()
 
 
 // PROTECTED MEMBERS
-void FileHandler::Initialize(const string &sFilepath)
+void FileHandler::Initialize(const std::string &sFilepath)
 {
 	if(ReadFileData(sFilepath))
 	{
@@ -246,9 +246,9 @@ void FileHandler::Initialize(const string &sFilepath)
 	}
 }
 
-string FileHandler::ExtractFileName(const string &sFilePath)
+std::string FileHandler::ExtractFileName(const std::string &sFilePath)
 {
-	string sFileName;
+	std::string sFileName;
 	if(sFilePath.empty())
 	{
 		LOG_ERROR("Provided filepath is empty.");
@@ -256,8 +256,8 @@ string FileHandler::ExtractFileName(const string &sFilePath)
 	}
 	else
 	{
-		int matchIndex = sFilePath.rfind('\\') + 1;
-		if(matchIndex != string::npos)
+		unsigned int matchIndex = sFilePath.rfind('\\') + 1;
+		if(matchIndex != std::string::npos)
 		{
 			m_sFilePath = sFilePath.substr(0, matchIndex);
 			sFileName = sFilePath.substr(matchIndex);
@@ -276,14 +276,14 @@ void FileHandler::Release()
 	m_fstream.close();
 }
 
-bool FileHandler::ReadFileData(const string &sFilepath)
+bool FileHandler::ReadFileData(const std::string &sFilepath)
 {
 	// TODO:: Do Error Checking
 	m_fstream.open(sFilepath);
 
 	if(m_fstream.is_open())
 	{
-		string sFileLine;
+		std::string sFileLine;
 		while(getline(m_fstream, sFileLine))
 		{
 			m_vFileLines.push_back(sFileLine + "\n");
@@ -297,63 +297,70 @@ bool FileHandler::ReadFileData(const string &sFilepath)
 	}
 }
 
-int FileHandler::SearchLine(const string &sSrc, bool bCaseSensitiveSearch)
+int FileHandler::SearchLine(const std::string &sSrc, bool bCaseSensitiveSearch)
 {
 	if(bCaseSensitiveSearch)
 	{
-		vector<string>::iterator it = find(m_vFileLines.begin(), m_vFileLines.end(), sSrc);
+		std::vector<std::string>::iterator it = find(m_vFileLines.begin(), m_vFileLines.end(), sSrc);
 		return (it - m_vFileLines.begin());
 	}
 	else
 	{
-		string sSource;
+		std::string sSource;
 		transform(sSrc.begin(), sSrc.end(), sSource.begin(), ::tolower);
 
-		bool (*Compare)(string, string) = bind(CompareCaseInSensitive, sSource);
+		struct CompareIt compareIt;
 
-		vector<string>::iterator it;
-		find_if(m_vFileLines.begin(), m_vFileLines.end(), Compare);
+		std::function<bool (std::string)> compareFunc = std::bind(&CompareIt::CompareCaseInSensitive, &compareIt, sSource, std::placeholders::_1);
+
+		std::vector<std::string>::iterator it;
+		find_if(m_vFileLines.begin(), m_vFileLines.end(), compareFunc);
 		return (it - m_vFileLines.begin());
 	}
 	return 0;
 }
 
-bool FileHandler::SearchLine(const string &sSrc, vector<int> &v_SearchIndexes, bool bCaseSensitiveSearch)
+bool FileHandler::SearchLine(const std::string &sSrc, std::vector<int> &v_SearchIndexes, bool bCaseSensitiveSearch)
 {
+	std::string sSource;
 	bool bRc = true;
-	string sTarget;
+
 	if(bCaseSensitiveSearch)
 	{
-		sTarget = sSrc;
+		sSource = sSrc;
 	}
 	else
 	{
-		transform(sSrc.begin(), sSrc.end(), sTarget.begin(), ::tolower);
+		std::transform(sSrc.begin(), sSrc.end(), sSource.begin(), ::tolower);
 	}
 
 	// MainCode for searching
+	struct CompareIt compareIt;
+	std::function<bool (std::string)> compareFunc = std::bind(&CompareIt::CompareCaseInSensitive, &compareIt, sSource, std::placeholders::_1);
+
+	// use generate or foreach
 	return bRc;
 }
 
 void FileHandler::Display() const
 {
 	size_t size = GetSize();
-	cout << "FileName       := " << m_sFileName;
-	cout << "\nFilPath        := " << m_sFilePath;
-	cout << "\nNumber of line := " << size;
-	cout << "\n\nFile           :=\n\n******************* START ********************\n";
+	std::cout << "FileName       := " << m_sFileName;
+	std::cout << "\nFilPath        := " << m_sFilePath;
+	std::cout << "\nNumber of line := " << size;
+	std::cout << "\n\nFile           :=\n\n******************* START ********************\n";
 
 
 	for(size_t i = 0; i < size; i++)
 	{
-		cout << GetLine(i);
+		std::cout << GetLine(i);
 	}
 
-	cout << "******************** END *********************" << endl;
+	std::cout << "******************** END *********************" << std::endl;
 }
 
 // PROTECTED FRIEND FUNCTION
-ostream& operator<< (ostream &out, const FileHandler &fileHandler)
+std::ostream& operator<< (std::ostream &out, const FileHandler &fileHandler)
 {
 	fileHandler.Display();
 	return out;
@@ -362,18 +369,17 @@ ostream& operator<< (ostream &out, const FileHandler &fileHandler)
 // PRIVATE MEMBERS
 bool FileHandler::ValidRange(int lineNumber) const
 {
-	return (lineNumber >=0 && lineNumber < GetSize());
+	return (lineNumber >=0 && static_cast<unsigned int>(lineNumber) < GetSize());
 }
 
-string FileHandler::GetFileNameFormRoot()
+std::string FileHandler::GetFileNameFormRoot()
 {
 	return m_sFilePath + "\\" + m_sFileName;
 }
 
-bool FileHandler::CompareCaseInSensitive(std::string sFirst, std::string sSecond)
+bool FileHandler::CompareIt::CompareCaseInSensitive(std::string sFirst, std::string sSecond)
 {
-	string sTarget;
-	transform(sSecond.begin(), sSecond.end(), sTarget.begin(), ::tolower);
-
+	std::string sTarget;
+	std::transform(sSecond.begin(), sSecond.end(), sTarget.begin(), ::tolower);
 	return (sFirst == sSecond);
 }
