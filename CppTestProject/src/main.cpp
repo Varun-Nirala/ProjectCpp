b90bc3b5f95df1cc25215dc2615e35a14a098078
Varun Nirala <nirala.varun@gmail.com>
2018-11-18 18:22:15 +0530

    Adding files

---
+++ CppTestProject/src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+/*template <typename T>
+typedef struct Node
+{
+	Node(T value = 0) :m_data(value)
+	{}
+
+	T m_data;
+};
+
+template<typename T>
+typedef struct BNode : public Node<T>
+{
+	BNode(T value = 0) : Node <T>(value)
+	{}
+
+	BNode *m_pL;	// Left Child
+	BNode *m_pR;	// Right Child
+	BNode *m_pP;	// Parent node
+};*/
+
+
+int main()
+{
+
+	return 0;
+}
