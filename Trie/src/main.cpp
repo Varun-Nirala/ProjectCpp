5003974fb2a85e80b62ad6308b2f525ed3612907
Varun Nirala <nirala.varun@gmail.com>
2020-05-22 16:10:27 +0530

    GitHub Varun : Trie -> adding main.cpp

---
+++ Trie/src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "trieTree.h"
+
+using namespace std;
+
+int main()
+{
+	DS::Trie trie;
+
+	trie.insert("to");
+	trie.insert("tea");
+	trie.insert("team");
+	trie.insert("ted");
+	trie.insert("teaming");
+	trie.insert("ten");
+	trie.insert("in");
+	trie.insert("inn");
+	trie.insert("india");
+
+	cout << trie << endl;
+
+	auto allWords = trie.getAllWordWithPrefix("i");
+	for_each(std::begin(allWords), std::end(allWords), [](auto &word) { cout << word << "\n"; });
+
+	return 0;
+}
