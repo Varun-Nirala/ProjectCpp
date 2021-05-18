76e8c1d3259a4f458a682df5694c57267e3a5356
Varun Nirala <nirala.varun@gmail.com>
2021-05-18 22:49:31 +0530

    Added some more methods

--- Testing/TGA_FIle.cpp
+++ Testing/TGA_FIle.cpp
@@ -4,18 +4,136 @@
 #include "TGA_File.h"
 #include "helper.h"
 
+namespace TGA {
 
-namespace ALL_USING
+using std::string;
+using std::cout;
+using std::endl;
+using std::vector;
+
+enum ImageType {
+	NO_IMAGE_DATA = 0,
+
+	UNCOMPRESSED_INDEX = 1,		// Not handling
+	UNCOMPRESSED_RGB = 2,
+	UNCOMPRESSED_GRAY = 3,
+
+	RLE_INDEXED = 9,			// Not handling
+	RLE_RGB = 10,
+	RLE_GRAY = 11,
+};
+
+void read1byte(const char const* srcbuffer, int& index, uint8_t& dst)
 {
-	using std::string;
-	using std::cout;
-	using std::endl;
-	using std::vector;
+	memcpy(&dst, &srcbuffer[index++], 1);
 }
 
-using namespace ALL_USING;
+void read2byte(const char const* srcbuffer, int& index, uint16_t& dst)
+{
+	memcpy(&dst, &srcbuffer[index], 2);
+	index += 2;
+}
 
-// PUBLIC MEMBERS
+void TGAHeader::parse(const char const* buffer)
+{
+	int index = 0;
+	read1byte(buffer, index, m_idLength);
+	read1byte(buffer, index, m_colorMapType);
+	read1byte(buffer, index, m_imageType);
+
+	read2byte(buffer, index, m_colorMapOrigin);
+	read2byte(buffer, index, m_colorMapLength);
+
+	read1byte(buffer, index, m_ColorMapbitsPerPixel);
+
+	read2byte(buffer, index, m_xOrigin);
+	read2byte(buffer, index, m_yOrigin);
+
+	read2byte(buffer, index, m_width);
+	read2byte(buffer, index, m_height);
+
+	read1byte(buffer, index, m_bitsPerPixel);
+	read1byte(buffer, index, m_imageDescriptor);
+}
+
+bool TGAHeader::isUnCompressed() const
+{
+	return m_imageType == UNCOMPRESSED_INDEX || m_imageType == UNCOMPRESSED_RGB || m_imageType == UNCOMPRESSED_GRAY;
+}
+
+bool TGAHeader::isRLE() const
+{
+	return m_imageType == RLE_INDEXED || m_imageType == RLE_RGB || m_imageType == RLE_GRAY;
+}
+
+bool TGAHeader::isIndexed() const
+{
+	return m_imageType == UNCOMPRESSED_INDEX || m_imageType == RLE_INDEXED;
+}
+
+bool TGAHeader::isRGB() const
+{
+	return m_imageType == RLE_RGB || m_imageType == UNCOMPRESSED_RGB;
+}
+
+bool TGAHeader::isGrayScale() const
+{
+	return m_imageType == UNCOMPRESSED_GRAY || m_imageType == RLE_GRAY;
+}
+
+int TGAHeader::bytesPerPixel() const
+{
+	return isRGB() ? 4 : 1;
+}
+
+bool TGAHeader::leftToRightOrder() const
+{
+	return (m_imageDescriptor & 0x10);
+}
+
+bool TGAHeader::topToBottomOrder() const
+{
+	return (m_imageDescriptor & 0x20);
+}
+
+int TGAHeader::getRowStride() const
+{
+	return m_width * bytesPerPixel();
+}
+
+int TGAHeader::getXoffset() const
+{
+	return leftToRightOrder() ? 1 : -1;
+}
+
+int TGAHeader::getYoffset() const
+{
+	return topToBottomOrder() ? 1 : -1;
+}
+
+int TGAHeader::getImageSize() const
+{
+	return getRowStride() * m_height
+}
+
+void TGAHeader::display() const
+{
+	std::cout << "ID length         :: " << (int)m_idLength << '\n';
+	std::cout << "Color Map Type    :: " << (int)m_colorMapType << '\n';
+	std::cout << "Image Type        :: " << (int)m_imageType << '\n';
+
+	std::cout << "Color Map Origin  :: " << (int)m_colorMapOrigin << '\n';
+	std::cout << "Color Map Length  :: " << (int)m_colorMapLength << '\n';
+	std::cout << "Color Map Depth   :: " << (int)m_ColorMapbitsPerPixel << '\n';
+
+	std::cout << "X Origin          :: " << (int)m_xOrigin << '\n';
+	std::cout << "Y Origin          :: " << (int)m_yOrigin << '\n';
+	std::cout << "Width in pixel    :: " << (int)m_width << '\n';
+	std::cout << "Height in pixel   :: " << (int)m_height << '\n';
+	std::cout << "Bits per Pixel    :: " << (int)m_bitsPerPixel << '\n';
+	std::cout << "Image Descriptor  :: " << (int)m_imageDescriptor << '\n';
+}
+
 TGAFile::TGAFile(const string &sFilepath)
 {
 	initialize(sFilepath);
@@ -23,7 +141,6 @@ TGAFile::TGAFile(const string &sFilepath)
 
 TGAFile::~TGAFile()
 {
-	release();
 }
 
 string TGAFile::getFileName() const
@@ -36,52 +153,12 @@ string TGAFile::getFilePath() const
 	return m_sFilePath;
 }
 
-size_t TGAFile::getSize() const
-{
-	return m_size;
-}
-
-//string TGAFile::getLine(int lineNumber) const
-//{
-//	if(ValidRange(lineNumber))
-//	{
-//		return m_vFileLines[lineNumber];
-//	}
-//	else
-//	{
-//		// TODO::LOG_ERROR
-//		LOG_ERROR("Provided linenumber is not in valid range.");
-//	}
-//	return "";
-//}
-//
-//bool TGAFile::saveEditedData()
-//{
-//	// Empty files current content
-//	std::ofstream fileStream;
-//	fileStream.open(GetFileNameFormRoot(), std::ofstream::out | std::ofstream::trunc);
-//	if(fileStream.is_open())
-//	{
-//		for(const std::string &sFileLines : m_vFileLines)
-//		{
-//			fileStream << sFileLines;
-//		}
-//		fileStream.close();
-//	}
-//	return true;
-//}
-
-
 // PROTECTED MEMBERS
 void TGAFile::initialize(const string &sFilepath)
 {
 	if(parse(sFilepath))
 	{
 		m_sFileName = extractFileName(sFilepath);
-	}
-	else
-	{
-		m_pixels.clear();
 	}
 }
 
@@ -109,12 +186,120 @@ string TGAFile::extractFileName(const string &sFilePath)
 	return sFileName;
 }
 
-void TGAFile::release()
+bool TGAFile::parse(const string& sFilepath)
 {
-	//saveEditedData();
+	char* buffer = nullptr;
+	int length = readFileInBuffer(sFilepath, buffer);
+
+	if (length == -1)
+	{
+		return false;
+	}
+
+	readVersion(buffer, length);
+
+	m_header.parse(buffer);
+
+	int index = 18;	// as we have already read the header
+
+	// Read image id
+	if (m_header.m_idLength > 0)
+	{
+		int size = m_header.m_idLength;
+		uint8_t val;
+		while (size--)
+		{
+			read1byte(buffer, index, val);
+			m_imageID.push_back((char)val);
+		}
+	}
+
+	// Need to parse color Map
+	if (m_header.m_colorMapType == 1)
+	{
+		// No color map data
+		//parseColorMap(buffer, index);
+	}
+	else
+	{
+		readPixelData(buffer, index);
+	}
+
+	delete[] buffer;
+	return true;
 }
 
-bool TGAFile::parse(const string& sFilepath)
+//void TGAFile::parseColorMap(const char const* buffer, int& index){}
+
+void TGAFile::readPixelData(const char const* buffer, int& index)
+{
+	switch (m_header.m_imageType)
+	{
+		case UNCOMPRESSED_INDEX:
+			// read as 1 byte
+			LOG_ERROR("Error: currently UNCOMPRESSED_INDEX is not handled.");
+			break;
+		case UNCOMPRESSED_RGB:
+			// read as 4 bytes
+			readUnCompressedData(buffer, index);
+			break;
+		case UNCOMPRESSED_GRAY:
+			LOG_ERROR("Error: currently UNCOMPRESSED_GRAY is not handled.");
+			// read as 1 byte
+			break;
+
+		case RLE_INDEXED:
+			LOG_ERROR("Error: currently RLE_INDEXED is not handled.");
+			// read as 1 byte
+			break;
+		case RLE_RGB:
+			readRleData(buffer, index);
+			// read as 4 bytes
+			break;
+		case RLE_GRAY:
+			// read as 1 byte
+			LOG_ERROR("Error: currently RLE_GRAY is not handled.");
+			break;
+		default:
+			LOG_ERROR("Error: Image type unkown.");
+	}
+}
+
+void TGAFile::readUnCompressedData(const char const* buffer, int& index)
+{
+	m_vPixels.resize(m_header.getImageSize());
+
+	int xOffset = m_header.getXoffset();
+	int yOffset = m_header.getYoffset();
+
+	int x = (xOffset == 1 ? 0 : m_header.m_width - 1);
+	int y = (yOffset == 1 ? 0 : m_header.m_height - 1);
+
+	for (int i = 0; i < m_header.m_height; ++i)
+	{
+		for (int j = 0; j < m_header.m_width; ++j)
+		{
+			uint8_t r, g, b, a;
+			m_vPixels.push_back(r);
+			m_vPixels.push_back(g);
+			m_vPixels.push_back(b);
+			m_vPixels.push_back(a);
+		}
+	}
+}
+
+void TGAFile::readRleData(const char const* buffer, int& index)
+{
+	m_vPixels.resize(m_header.getImageSize());
+
+	int xOffset = m_header.getXoffset();
+	int yOffset = m_header.getYoffset();
+
+	int x = (xOffset == 1 ? 0 : m_header.m_width - 1);
+	int y = (yOffset == 1 ? 0 : m_header.m_height - 1);
+}
+
+int TGAFile::readFileInBuffer(const std::string& sFilepath, char *& buffer) const
 {
 	std::ifstream file(sFilepath, std::ios::in | std::ios::binary | std::ios::ate);
 
@@ -122,14 +307,14 @@ bool TGAFile::parse(const string& sFilepath)
 	{
 		LOG_ERROR("Error: Opening file :: " + sFilepath + "\n");
 		file.close();
-		return false;
+		return -1;
 	}
 
 	file.seekg(0, file.end);
 	int length = file.tellg();
 	file.seekg(0, file.beg);
 
-	char* buffer = new char[length];
+	buffer = new char[length];
 
 	file.read(buffer, length);
 
@@ -138,47 +323,15 @@ bool TGAFile::parse(const string& sFilepath)
 		LOG_ERROR("Error: Only :: " + std::to_string(file.gcount()) + " could be read" + "\n");
 	}
 	file.close();
+}
 
-	TGAHeader header;
-	header.parse(buffer);
-
-	header.display();
-
+void TGAFile::readVersion(const char const* buffer, int length)
+{
 	char singature[17]{};
-
 	memcpy(singature, &buffer[length - 18], 16);
-
 	if (strncmp("TRUEVISION-XFILE", singature, 16) == 0)
 	{
 		m_version = 2;
 	}
-
-	return true;
 }
-
-void TGAFile::display() const
-{
-	size_t size = getSize();
-	cout << "FileName         := " << m_sFileName << '\n';
-	cout << "FilPath          := " << m_sFilePath << '\n';
-	cout << "Number of line   := " << size << "\n\n";
-	cout << "File             :=\n\n******************* START ********************\n";
-
-	for(size_t i = 0; i < size; i++)
-	{
-		cout << m_pixels[i];
-	}
-
-	cout << "******************** END *********************" << endl;
-}
-
-// PRIVATE MEMBERS
-bool TGAFile::validRange(int lineNumber) const
-{
-	return (lineNumber >= 0 && static_cast<unsigned int>(lineNumber) < getSize());
-}
-
-string TGAFile::getFileNameFormRoot()
-{
-	return m_sFilePath + "\\" + m_sFileName;
 }
--- Testing/TGA_File.h
+++ Testing/TGA_File.h
@@ -4,137 +4,96 @@
 #include <fstream>
 #include <vector>
 
-
-struct TGAHeader
+namespace TGA
 {
-	// Header contains top 5 field in total of 18 bytes of data
+	void read1byte(const char const* srcbuffer, int& index, uint8_t& dst);
+	void read2byte(const char const* srcbuffer, int& index, uint16_t& dst);
 
-	uint8_t		m_idLength;						// Length of image ID field
-	uint8_t		m_colorMapType;					// Whether a color map is included, 0 = No color map, 1 = Color map present
-	uint8_t		m_imageType;					// Compression and color type
+	class TGAFile;
+	class TGAPixelData;
 
-	// Color Map specification 5 bytes, Describes the color map
-	uint16_t	m_colorMapOrigin;				// index of first color map entry that is included in the file
-	uint16_t	m_colorMapLength;				// number of entries of the color map that are included in the file
-	uint8_t		m_ColorMapbitsPerPixel;			// number of bits per pixel, typically 15, 16, 24 or 32 bit per value
-
-	// Image specification 10 bytes, Describes the image dimensions and format
-	uint16_t	m_xOrigin;						// absolute horizontal coordinate
-	uint16_t	m_yOrigin;						// absolute vertical coordinate
-	uint16_t	m_width;						// width in pixels
-	uint16_t	m_height;						// height in pixels
-	uint8_t		m_bitsPerPixel;					// bits per pixel [24, 32]
-	uint8_t		m_imageDescriptor;				// bits 0-3 give the alpha channel depth, bits 5-4 give direction
-
-	void parse(const char const* buffer)
+	class TGAHeader
 	{
-		int index = 0;
-		memcpy(&m_idLength, &buffer[index++], 1);
-		memcpy(&m_colorMapType, &buffer[index++], 1);
-		memcpy(&m_imageType, &buffer[index++], 1);
-
-		memcpy(&m_colorMapOrigin, &buffer[index], 2);
-		index += 2;
-
-		memcpy(&m_colorMapLength, &buffer[index], 2);
-		index += 2;
-
-		memcpy(&m_ColorMapbitsPerPixel, &buffer[index++], 1);
-
-		memcpy(&m_xOrigin, &buffer[index], 2);
-		index += 2;
-
-		memcpy(&m_yOrigin, &buffer[index], 2);
-		index += 2;
-
-		memcpy(&m_width, &buffer[index], 2);
-		index += 2;
-
-		memcpy(&m_height, &buffer[index], 2);
-		index += 2;
-
-		memcpy(&m_bitsPerPixel, &buffer[index++], 1);
-
-		memcpy(&m_imageDescriptor, &buffer[index++], 1);
-	}
-
-	void display()
-	{
-		std::cout << "ID length         :: " << (int)m_idLength << '\n';
-		std::cout << "Color Map Type    :: " << (int)m_colorMapType << '\n';
-		std::cout << "Image Type        :: " << (int)m_imageType << '\n';
-
-		std::cout << "Color Map Origin  :: " << (int)m_colorMapOrigin << '\n';
-		std::cout << "Color Map Length  :: " << (int)m_colorMapLength << '\n';
-		std::cout << "Color Map Depth   :: " << (int)m_ColorMapbitsPerPixel << '\n';
-
-		std::cout << "X Origin          :: " << (int)m_xOrigin << '\n';
-		std::cout << "Y Origin          :: " << (int)m_yOrigin << '\n';
-		std::cout << "Width in pixel    :: " << (int)m_width << '\n';
-		std::cout << "Height in pixel   :: " << (int)m_height << '\n';
-		std::cout << "Bits per Pixel    :: " << (int)m_bitsPerPixel << '\n';
-		std::cout << "Image Descriptor  :: " << (int)m_imageDescriptor << '\n';
-	}
-};
-
-enum class ImageType {
-	NO_IMAGE_DATA = 0,
-
-	UNCOMPRESSED_COLOR_MAPPED = 1,
-	UNCOMPRESSED_TRUE_COLOR = 2,
-	UNCOMPRESSED_BLACK_WHITE = 3,
-
-	RLE_COLOR_MAPPED = 9,
-	RLE_TRUE_COLOR = 10,
-	RLE_BLACK_WHITE = 11,
-};
-
-class TGAFile
-{
+		friend TGAFile;
+		friend TGAPixelData;
 	public:
-		TGAFile(const std::string &sFilepath);
+		void parse(const char const* buffer);
+
+		bool isUnCompressed() const;
+		bool isRLE() const;
+
+		bool isIndexed() const;
+		bool isRGB() const;
+		bool isGrayScale() const;
+
+		int bytesPerPixel() const;
+
+		bool leftToRightOrder() const;
+		bool topToBottomOrder() const;
+
+		int getRowStride() const;
+		int getXoffset() const;
+		int getYoffset() const;
+
+		int getImageSize() const;	// returns total number of pixels
+		
+		void display() const;
+
+	private:
+		// Header contains top 5 field in total of 18 bytes of data
+
+		uint8_t		m_idLength;						// Length of image ID field
+		uint8_t		m_colorMapType;					// Whether a color map is included, 0 = No color map, 1 = Color map present
+		uint8_t		m_imageType;					// Compression and color type
+
+		// Color Map specification 5 bytes, Describes the color map
+		uint16_t	m_colorMapOrigin;				// index of first color map entry that is included in the file
+		uint16_t	m_colorMapLength;				// number of entries of the color map that are included in the file
+		uint8_t		m_ColorMapbitsPerPixel;			// number of bits per pixel, typically 15, 16, 24 or 32 bit per value
+
+		// Image specification 10 bytes, Describes the image dimensions and format
+		uint16_t	m_xOrigin;						// absolute horizontal coordinate
+		uint16_t	m_yOrigin;						// absolute vertical coordinate
+		uint16_t	m_width;						// width in pixels
+		uint16_t	m_height;						// height in pixels
+		uint8_t		m_bitsPerPixel;					// bits per pixel [24, 32]
+		uint8_t		m_imageDescriptor;				// bits 0-3 give the alpha channel depth, bits 5-4 give direction
+	};
+
+	class TGAFile
+	{
+	public:
+		TGAFile(const std::string& sFilepath);
 		~TGAFile();
 
 		std::string getFileName() const;
 		std::string getFilePath() const;
 
-		size_t getSize() const;
-		/*std::string getLine(int lineNumber) const;*/
-
-		/*bool saveEditedData();*/
-
-		// FRIEND FUNCTIONS
-		friend std::ostream& operator<< (std::ostream& out, const TGAFile& fileHandler)
-		{
-			fileHandler.display();
-			return out;
-		}
-
 	protected:
-		void initialize(const std::string &sFilepath);
-		std::string extractFileName(const std::string &sFilePath);
+		void initialize(const std::string& sFilepath);
+		std::string extractFileName(const std::string& sFilePath);
 
-		void release();
-		bool parse(const std::string &sFilepath);
+		bool parse(const std::string& sFilepath);
 
-		virtual void display() const;
+		//void parseColorMap(const char const* buffer, int& index);
+
+		void readPixelData(const char const* buffer, int& index);
+
+		void readUnCompressedData(const char const* buffer, int& index);
+
+		void readRleData(const char const* buffer, int& index);
+
+		int readFileInBuffer(const std::string& sFilepath, char*& buffer) const;
+		void readVersion(const char const* buffer, int length);
 
 	private:
-		bool validRange(int lineNumber) const;
-		std::string getFileNameFormRoot();
-
-	private:
-		std::vector<uint8_t>			m_pixels;
 		std::string 					m_sFileName;
 		std::string 					m_sFilePath;
-		size_t							m_width{};	// In pixel
-		size_t							m_height{};	// In pixel
 
-		size_t							m_size{};
-
-		size_t							m_bitsPerPixel{};
-		bool							m_bIsCompressed{ true };
-		int								m_version{1};
-};
-
+		int								m_version{ 1 };
+		std::string						m_imageID;
+		TGAHeader						m_header;
+		std::vector<uint8_t>			m_vPixels;
+	};
+}
 #endif		//#define __FILEHANDLER_H__
